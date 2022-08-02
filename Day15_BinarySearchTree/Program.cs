812643782b2ea742626e7371d38b8dbfa4774a59
karishmask46 <karishmask.15999@gmail.com>
2022-08-02 22:05:30 +0530

    creating binary search tree

---
+++ Day15_BinarySearchTree/Program.cs
@@ -0,0 +1,7 @@
+﻿using Day15_BinarySearchTree;
+Console.WriteLine("welcome to the binary search tree!"); 
+BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+binarySearch.Insert(30);
+binarySearch.Insert(70);
+binarySearch.Display();
+
